a41d11ecbe1adaaa61d3f4e6f85b50fa19959378
SCSFMAC2 <scsfmac2@SCSFMAC2s-MacBook-Air.local>
2016-10-26 18:00:34 -0700

    10-26-16 end

--- assets/scripts/part_one_responses.js
+++ assets/scripts/part_one_responses.js
@@ -1,1 +1,15 @@
-kadjlshfhfxa
+var thisYear = 2016; 
+var thisMonth = 10;
+var dividedBy = thisYear / thisMonth; 
+var remainder = 279 % 7; 
+var eightteenSquared = 18 * 18; 
+var fakeStreet = '123 Market Street';  
+var thisCity = 'San Francisco';
+var thisState = 'California'; 
+var fakeAddress = fakeStreet + ' ' + thisCity + ', ' + thisState;
+function greet () {
+	return 'Hello World!';
+}
+
+
+
